64696223e51c8d935bf7ac254f2fe8e8c63ec9fb
ShubhamAgarwal1616 <shubham@mavenhive.in>
2019-02-06 18:06:45 +0530

    modify for controlled input

--- src/__test__/components/bmi-calculator.test.js
+++ src/__test__/components/bmi-calculator.test.js
@@ -1,15 +1,14 @@
-import {mount} from 'enzyme';
-import React from 'react';
-import BmiCalculator from '../../components/bmi_calculator';
-import '../../../setupTests'
+import { mount } from "enzyme";
+import React from "react";
+import BmiCalculator from "../../components/bmi_calculator";
+import "../../../setupTests";
 
 describe("Bmi_calculator", () => {
   let componentRoot;
   beforeEach(() => {
-    componentRoot = mount(<BmiCalculator/>);
+    componentRoot = mount(<BmiCalculator />);
   });
   it("renders a root element with a class name bmi-calculator", () => {
-
     expect(componentRoot.exists("div.bmi-calculator")).toBeTruthy();
   });
 
@@ -27,15 +26,14 @@ describe("Bmi_calculator", () => {
 
   it("expects text of button to Get Bmi", () => {
     expect(componentRoot.find("button.get-bmi").text()).toBe("GET BMI");
-    expect(componentRoot.exists("button.get-bmi")).toBeTruthy();
   });
 
-  it("checks the clicking functionality of button", () => {
-    let height = componentRoot.find("input.height").instance();
-    let weight = componentRoot.find("input.weight").instance();
-    height.value=10;
-    weight.value=100;
-    let button=componentRoot.find("button.get-bmi");
+  it("checks the bmi index is 1", () => {
+    let height = componentRoot.find("input.height");
+    let weight = componentRoot.find("input.weight");
+    height.simulate("change", { target: { value: 10 } });
+    weight.simulate("change", { target: { value: 100 } });
+    let button = componentRoot.find("button.get-bmi");
     button.simulate('click');
     expect(componentRoot.find("span.bmi").text()).toEqual("1");
   });
--- src/components/bmi_calculator.js
+++ src/components/bmi_calculator.js
@@ -4,29 +4,50 @@ export default class BmiCalculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      height: "",
+      weight: "",
       bmi: ""
-    }
-    this.height = React.createRef();
-    this.weight = React.createRef();
-    this.computeBmi = this.computeBmi.bind(this);
+    };
+    this.height_in_meter = this.height_in_meter.bind(this);
+    this.weight_in_kilogram = this.weight_in_kilogram.bind(this);
+    this.compute = this.compute.bind(this);
   }
 
   render() {
     return (
       <div className="bmi-calculator" align="center">
-        <input className="height" ref={this.height}/><br />
-        <input className="weight" ref={this.weight}/><br />
-        <button className="get-bmi" onClick={this.computeBmi}>GET BMI</button>
+        <input
+          className="height"
+          value={this.state.height}
+          onChange={this.height_in_meter}
+          placeholder={"height"}
+        />
+        <br/>
+        <input
+          className="weight"
+          value={this.state.weight}
+          onChange={this.weight_in_kilogram}
+          placeholder={"weight"}
+        />
+        <br/>
+        <button className="get-bmi" onClick={this.compute}>
+          GET BMI
+        </button>
         <span className="bmi">{this.state.bmi}</span>
       </div>
     );
   }
 
-  computeBmi() {
-    let weight = this.weight.current.value;
-    let height = this.height.current.value * this.height.current.value;
-    let bmi = weight / height;
-    this.setState({bmi: bmi})
+  height_in_meter(h) {
+    this.setState({height: h.target.value});
+  }
+
+  weight_in_kilogram(w) {
+    this.setState({weight: w.target.value});
+  }
+
+  compute() {
+    let bmi = (this.state.weight / this.state.height ** 2);
+    this.setState({bmi: bmi});
   }
 }
-
